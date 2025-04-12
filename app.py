54f08d0187570d538bdaa15a20b675a46650b731
JuanMarin05 <juanjosemarinpino05@gmail.com>
2025-04-13 00:03:08 +0200

    minor updates and database setup

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-from flask import Flask, render_template
+import toml
+from flask import Flask, render_template, request, flash, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text, func
 
 app = Flask(__name__)
+app.config.from_file("config.toml", load=toml.load)
+app.secret_key = app.config['SECRET_KEY']
+
+db = SQLAlchemy()
+db.init_app(app)
 
 # Ruta para la página inicial
 @app.route('/')
@@ -11,6 +19,11 @@ def index():
 @app.route('/chat')
 def chat():
     return render_template('chat.html')
+
+# Ruta para la página del perfil de usuario
+@app.route('/perfil')
+def perfil():
+    return render_template('perfil.html')
 
 # Ruta para la página de login
 @app.route('/login')
