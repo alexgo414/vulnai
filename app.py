e1388535aafbb1e5785df1ee33841e1f95c2071a
JuanMarin05 <juanjosemarinpino05@gmail.com>
2025-04-25 20:14:32 +0200

    administracion usuarios modo admin

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request, flash, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text, func
 from datetime import date
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.config.from_file("config.toml", load=toml.load)
@@ -20,6 +21,14 @@ class Proyecto(db.Model):
     descripcion = db.Column(db.Text, nullable=True)
     fecha_creacion = db.Column(db.Date, nullable=False)
     fecha_modificacion = db.Column(db.Date, nullable=False)
+
+class Usuario(db.Model):
+    __tablename__ = 'usuarios'
+    id = db.Column(db.String(36), primary_key=True)
+    nombre = db.Column(db.String(20), nullable=False)
+    apellidos = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    password = db.Column(db.String(200), nullable=False)
 
 # Crear la tabla proyectos en la base de datos
 with app.app_context():
@@ -39,7 +48,8 @@ def chat():
 @app.route('/perfil')
 def perfil():
     proyectos = db.session.execute(db.select(Proyecto)).scalars().all()
-    return render_template('perfil.html', proyectos=proyectos)
+    usuarios = db.session.execute(db.select(Usuario)).scalars().all()
+    return render_template('perfil.html', proyectos=proyectos, usuarios=usuarios)
 
 # Ruta para la página de login
 @app.route('/login')
@@ -56,7 +66,7 @@ def proyecto_nuevo():
         # Obtener los datos del formulario
         nombre = request.form['nombre']
         descripcion = request.form['descripcion']
-        fecha_creacion = request.form['fecha_creacion']
+        fecha_creacion = date.today().strftime("%Y-%m-%d")
         if fecha_creacion != date.today().strftime("%Y-%m-%d"):
             fecha_modificacion = date.today().strftime("%Y-%m-%d")
         else:
@@ -86,12 +96,70 @@ def proyecto_editar(id=None):
     else:
         return render_template("proyecto_editar.html", proyecto=p)
 
+# Ruta para eliminar un proyecto
 @app.route("/proyecto_eliminar/<string:id>")
 def proyecto_eliminar(id=None):
     p = db.one_or_404(db.select(Proyecto).where(Proyecto.id == id))
     db.session.delete(p)
     db.session.commit()
     flash(f"Proyecto <em>{p.nombre}</em> eliminado con éxito.", "exito")
+    return redirect(url_for("perfil"))
+
+# Ruta para crear un nuevo usuario
+@app.route('/perfil/usuario_nuevo', methods=['GET', 'POST'])
+def usuario_nuevo():
+    if request.method == "POST":
+        # Generar un ID único automáticamente
+        id = str(uuid.uuid4())
+
+        # Obtener los datos del formulario
+        nombre = request.form['nombre']
+        apellidos = request.form['apellidos']
+        email = request.form['email']
+
+        # Obtener la contraseña y encriptarla
+        password = request.form['password']
+        hashed_password = generate_password_hash(password, method='pbkdf2:sha256', salt_length=16)
+
+        u = Usuario(id=id, nombre=nombre, apellidos=apellidos, email=email, password=hashed_password)
+
+        # Guardar el nuevo proyecto en la base de datos
+        db.session.add(u)
+        db.session.commit()
+        flash(f"Usuario <em>{nombre}</em> añadido con éxito.", "exito")
+        return redirect(url_for("perfil"))
+    else:
+        return render_template("usuario_nuevo.html")
+    
+# Ruta para la página de editar usuario
+@app.route("/perfil/usuario_editar/<string:id>", methods=["GET", "POST"])
+def usuario_editar(id=None):
+    u = db.one_or_404(db.select(Usuario).where(Usuario.id == id))
+    if request.method == "POST":
+        u.nombre = request.form['nombre']
+        u.apellidos = request.form['apellidos']
+        u.email = request.form['email']
+
+        password = request.form['password']
+        # Obtener la contraseña y encriptarla
+        password = request.form['password']
+        hashed_password = generate_password_hash(password, method='pbkdf2:sha256', salt_length=16)
+        
+        u.password = hashed_password
+
+        db.session.commit()
+        flash(f"Usuario <em>{u.nombre}</em> modificado con éxito.", "exito")
+        return redirect(url_for("perfil"))
+    else:
+        return render_template("usuario_editar.html", usuario=u)
+
+# Ruta para eliminar un usuario
+@app.route("/usuario_eliminar/<string:id>")
+def usuario_eliminar(id=None):
+    u = db.one_or_404(db.select(Usuario).where(Usuario.id == id))
+    db.session.delete(u)
+    db.session.commit()
+    flash(f"Proyecto <em>{u.nombre}</em> eliminado con éxito.", "exito")
     return redirect(url_for("perfil"))
 
 if __name__ == '__main__':
